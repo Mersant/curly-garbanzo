d0a2325c8fa8ac3d151dd972b4046dd9995e9046
omarfarah123 <76454677+omarfarah123@users.noreply.github.com>
2022-10-12 11:18:27 -0400

    created typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,36 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+    _id: ID
+    username: String
+    password: String
+    messageHistory: [Messages]!
+  }
+
+  type Messages {
+    _id: ID
+    messageText: String
+    createdAt: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    messages(username: String): [Messages]
+    message(messageId: ID!): Messages
+  }
+ 
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addMessage(messageText: String!): Messages
+  }  
+`;
+
+module.exports = typeDefs;
